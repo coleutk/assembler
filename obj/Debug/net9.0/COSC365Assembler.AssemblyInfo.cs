c41e0bb13808505d73f1667f371a186c97d40928
coleutk <colepricecollege@gmail.com>
2025-04-03 15:28:48 -0400

    fully working

--- obj/Debug/net9.0/COSC365Assembler.AssemblyInfo.cs
+++ obj/Debug/net9.0/COSC365Assembler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COSC365Assembler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e02598346baf02ef1991d6101a6a834b995da264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d020ad7bf073b7cc998763b1c17348fc45687b70")]
 [assembly: System.Reflection.AssemblyProductAttribute("COSC365Assembler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COSC365Assembler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
